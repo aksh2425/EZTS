61d82020ed9190e55680b333d950ffe480f31be1
aksh2425 <129761322+aksh2425@users.noreply.github.com>
2024-02-20 23:25:43 +0530

    Add files via upload

---
+++ b.py
@@ -0,0 +1,101 @@
+#p1
+'''def fun(x):
+    x[0]=20
+l=[10,22,34,89]
+fun(l)
+print(l)'''
+#p2--sugar cane
+
+'''def e(x):
+    c=(x*50)
+    r=(30*c)//100
+    print(r)
+
+def t():
+    x=int(input())
+    while (x!=0):
+        n=int(input())
+        e(n)
+        x-=1
+t()'''
+#p3--watching movies
+'''def yt(a,b):
+    print ((b//2)+(a-b))
+def test():
+    x=int(input())
+    while(x!=0):
+        a,b=map(int,input().split())
+        yt(a,b)
+        x-=1
+test()'''
+#p4--
+'''def u(x,c):
+    if x==0:
+        return c
+    else:
+        if((x%10)==4):
+            c+=1
+        return u(x//10,c)
+for i in range(int(input())):
+    x=int(input())
+    print(u(x,0))'''
+#p4 app2
+'''def countt(x):
+    c=0
+    while x>0:
+        if(x%10==4):
+            c+=1
+        x=x//10
+    return x
+def t():
+    a=int(input())
+    while a!=0:
+        s=int(input())
+        countt(s)
+ t()'''
+#p5
+'''n=int(input())
+r=1
+while(n>0):
+    r=r*n
+    n-=1
+print(r)'''
+'''def f(x):
+    if x==1:
+        return 1
+    else:
+        return x*f(x-1)
+n=int(input())
+print(f(n))
+     '''
+#p6--harshu
+'''def count(x,c):
+    while(x!=0):
+        t=x%10
+        if t==4:
+            c=c+1
+        x=x//10
+    return c
+def testcases():
+    t=int(input())
+    while(t!=0):
+        t=t-1
+        x=int(input())
+        print(count(x,0))
+testcases() '''
+#p7--append three
+'''
+x=int(input())
+a=0
+d=x
+while(d!=0):
+    d//=10
+    a+=1
+x=(3*(10**a))+x
+x=(x*10)+3
+print(x)
+'''
+#p8
+
+
+    
